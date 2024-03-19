0cd445e5554f56ce55e8ef0a23cded969dedf95b
devlrakib <devrakib5@gmail.com>
2024-03-19 22:08:15 +0600

    first commit

---
+++ 18.8_practice_problem.js
@@ -0,0 +1,25 @@
+
+// ?===============================> Problem One Solved <===============================
+var moneyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+var totalCost = applePrice+orangePrice;
+var totalSum  = moneyGiven-totalCost;
+console.log(totalSum);
+
+// ?===============================> Problem Two Solved <===============================
+var mathmetics = 75.25;
+var biology    = 65;
+var chemistry  = 80;
+var physics    = 35.45;
+var bangla     = 99.50;
+var totalMarks = mathmetics+biology+chemistry+physics+bangla;
+var average    = totalMarks/5;
+var averageMarks = average.toFixed(2);
+console.log(averageMarks);
+
+// ?===============================> Problem Third Solved <===============================
+var number = 119;
+var reminder = number%5;
+console.log(reminder);
+
---
+++ 18.9_if_else.js
@@ -0,0 +1,31 @@
+
+// ? ================================> If,else Problem number one Solved <==============================
+var num1 = 13;
+var num2 = 79;
+var num3 = 45;
+
+if (num1 > num2){
+    if(num3 > num1){
+        console.log(num1);
+    }
+    else{
+        console.log(num2);
+    }
+}
+else{
+    if(num2 > num3){
+        console.log(num2);
+    }
+    else{
+        console.log(num3);
+    }
+}
+
+
+// ? ================================> If,else Problem number Two Solved <==============================
+var side1 = 9;
+var side2 = 8;
+var side3 = 9;
+if (side1 == side2 || side2 == side3 || side1 == side3){
+    console.log('Icolese');
+}
